40c12d0c2854fdd0324a29bcca32ffdd2fd93bbb
belalhaiss <belalhaiss@gmail.com>
2021-07-31 02:28:08 +0200

    [deploy]app

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
-
+const path = require('path');
 const { connectDB, sessionConfig } = require('./config/db');
 const cors = require('cors');
 
@@ -14,6 +14,13 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/orders', require('./routes/orders'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const port = process.env.PORT || 5000;
 
